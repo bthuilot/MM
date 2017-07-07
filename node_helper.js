01e4420ab116028e3feaf37a52ee48a557dc59b8
Bryce Thuilot <bthuilot@gmail.com>
2017-07-07 18:16:45 -0400

    I have no clue what the issue is. I think its that fetch doesn't work with the node on the raspberry pi

--- node_helper.js
+++ node_helper.js
@@ -1,20 +1,18 @@
 var NodeHelper = require("node_helper");
-var fetch = require('node-fetch');
 module.exports = NodeHelper.create({
 
   start: function(){
-    Log.info("Starting node helper for: " + this.name);
+    console.log("Starting node helper for: " + this.name);
   },
 
   socketNotificationReceived: function(notification, payload) {
-	   if(notification == "request-quote"){
-       fetch('https://www.reddit.com/r/quotes/random/.json')
-         .then(function(res) {
-               return res.json();
-         }).then(function(json) {
-             var rawQuote = json[0]['data']['children'][0]['data']['title'];
-             this.sendSocketNotification('new-quote', rawQuote);
-         });
-     }
+    var fetch = require('node-fetch');
+    fetch('https://www.reddit.com/r/quotes/random/.json')
+      .then(function(res) {
+        return res.json();
+      }).then(function(json) {
+        var rawQuote = json[0]['data']['children'][0]['data']['title'];
+        this.sendSocketNotification('new-quote', rawQuote);
+      });
   },
 });
