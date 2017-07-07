f2218930ed9844b1f06279e69b82ad7ac2a668d2
Bryce Thuilot <bthuilot@gmail.com>
2017-07-07 13:23:55 -0400

    node_module didn't work. Attempting another aproach

--- MMM-QuotesFromReddit.js
+++ MMM-QuotesFromReddit.js
@@ -14,14 +14,34 @@ Module.register("MMM-QuotesFromReddit",{
 		}, this.config.updateInterval);
 	},
 
+	getQuote: function(){
+		var request = new XMLHttpRequest();
+		request.open('GET', 'https://www.reddit.com/r/quotes/random/.json', true);
+
+		request.onreadystatechange = function() {
+		  if (this.readyState === 4) {
+		    if (this.status >= 200 && this.status < 400) {
+		      // Success!
+		      return JSON.parse(this.responseText)[0]['data']['children'][0]['data']['title'];
+		    } else {
+		      // Error :(
+		    }
+		  }
+		};
+
+		request.send();
+		request = null;
+	},
+
 	// Override dom generator.
 	getDom: function() {
-		this.sendSocketNotification('request-quote', null);
+		/*this.sendSocketNotification('request-quote', null);
 		socketNotificationReceived: function(notification, payload) {
 			if(notification == "new-quote"){
 				var rawQuote = payload;
 			}
-		};
+		};*/
+		var rawQuote = this.getQuote();
 		var quote = document.createTextNode(rawQuote);
 		var wrapper = document.createElement("div");
 		wrapper.className = "thin xlarge bright";
