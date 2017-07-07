5bad7cc124b6c2a145aaff5b1da68e86e0fda12d
Bryce Thuilot <bthuilot@gmail.com>
2017-07-07 01:48:43 -0400

    Beginning Inital Test

--- MMM-QuotesFromReddit.js
+++ MMM-QuotesFromReddit.js
@@ -13,29 +13,17 @@ Module.register("MMM-QuotesFromReddit",{
 		setInterval(function() {
 			self.updateDom(self.config.fadeSpeed);
 		}, this.config.updateInterval);
-	}
+	},
 
-	getJSON: function(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status == 200) {
-        callback(null, xhr.response);
-      } else {
-        callback(status);
-      }
-    };
-    xhr.send();
-};
 	// Override dom generator.
 	getDom: function() {
-		request('https://www.reddit.com/r/quotes/random/.json', function (error, response, body) {
-  	if (!error && response.statusCode == 200) {
-     	var rawQuote = JSON.parse(body);
-  		}
-		})
+		var fetch = require('node-fetch');
+		fetch('https://www.reddit.com/r/quotes/random/.json')
+		    .then(function(res) {
+		        return res.json();
+		    }).then(function(json) {
+		        var rawQuote = json[0]['data']['children'][0]['data']['title'];
+		    });
 		var quote = document.createTextNode(rawQuote);
 		var wrapper = document.createElement("div");
 		wrapper.className = "thin xlarge bright";
