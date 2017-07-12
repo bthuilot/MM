361002b11d2a6a2292727f73a173e52eb45a668f
Bryce's Magic Mirror <magicmirror@brycethuilot.com>
2017-07-11 21:24:45 -0400

    Resetting my magic mirror because i think its all screwed up :/

--- node_helper.js
+++ node_helper.js
@@ -1,4 +1,4 @@
-var fetch = require('node-fetch');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var NodeHelper = require("node_helper");
 module.exports = NodeHelper.create({
 
@@ -7,12 +7,19 @@ module.exports = NodeHelper.create({
   },
 
   socketNotificationReceived: function(notification, payload) {
-    fetch('https://www.reddit.com/r/quotes/random/.json')
-      .then(function(res) {
-        return res.json();
-      }).then(function(json) {
-        var rawQuote = json[0]['data']['children'][0]['data']['title'];
-        this.sendSocketNotification('new-quote', rawQuote);
-      });
+    var xhr = new XMLHttpRequest();
+    var self = this;
+		xhr.open("GET", url, true);
+		xhr.onreadystatechange = function() {
+			if (this.readyState === 4) {
+				if (this.status === 200) {
+					this.sendSocketNotification('new-quote',  JSON.parse(this.response)[0]['data']['children'][0]['data']['title']);;
+				} else {
+					console.log(self.name + "Could not load Quotes but doesnt matter cause i cant trouble shoot.");
+				}
+			}
+		};
+		xhr.send();
+    //this.sendSocketNotification('new-quote', a[0]['data']['children'][0]['data']['title']);
   },
 });
