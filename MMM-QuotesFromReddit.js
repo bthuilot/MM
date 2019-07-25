07c2ac4c22d6ffbbd2ee7bd3ebe7504566bea83d
Bryce Thuilot <bthuilot@gmail.com>
2019-07-25 15:05:41 -0400

    Removing node_helper.js

--- MMM-QuotesFromReddit.js
+++ MMM-QuotesFromReddit.js
@@ -1,6 +1,6 @@
+var quote;
 Module.register("MMM-QuotesFromReddit",{
 
-  let quote;
 	// Default module config.
 	defaults: {
 		updateInterval: 30000,
@@ -13,23 +13,29 @@ Module.register("MMM-QuotesFromReddit",{
 		var self = this;
 		this.sendSocketNotification('request-quote', null);
 		setInterval(function() {
-			self.updateDom(self.config.fadeSpeed);
-		}, this.config.updateInterval);
+			self.getNewQuote();
+    }, this.config.updateInterval);
 	},
 
-	socketNotificationReceived: function(notification, payload) {
-		if(notification === "new-quote"){
-      this.quote = payload;
+  getNewQuote: function () {
+    const http = new XMLHttpRequest();
+    http.onreadystatechange = () => {
+      if (http.readyState == 4 && http.status == 200) {
+        const result = JSON.parse(http.responseText);
+        quote = result[0]['data']['children'][0]['data']['title']
+      }
     }
-	},
+    http.open('GET', URL);
+    http.send
+    self.updateDom(self.config.fadeSpeed);
+  },
 
 	// Override dom generator.
 	getDom: function() {
-		const quoteNode = document.createTextNode(this.quote || 'Loading...');
+		const quoteNode = document.createTextNode(quote || 'Loading...');
 		var wrapper = document.createElement("div");
-	  wrapper.className = `thin large ${this.quote ? 'bright' : 'dim'}`;
+	  wrapper.className = `thin large ${quote ? 'bright' : 'dim'}`;
 		wrapper.appendChild(quoteNode);
-		this.sendSocketNotification('request-quote', null);
 		return wrapper;
 	},
 });
